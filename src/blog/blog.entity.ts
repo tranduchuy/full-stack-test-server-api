079c38fa9f7cae8a7509a2bc01b69eafe71c2c4e
tranduchuy <thewish.it@gmail.com>
2021-11-20 15:46:17 +0700

    ADD init blog module

---
+++ src/blog/blog.entity.ts
@@ -0,0 +1,19 @@
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+
+@Entity({
+    name: 'blog'
+})
+export class Blog {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    content: string;
+
+    @Column()
+    imageSrc: string;
+}
